e63fb66232a54d0facc7eddee36e2ae55a05833a
wlydlut <100394679+wlydlut@users.noreply.github.com>
2023-08-16 22:10:19 +0800

    Update test_real_denoising_sidd.py

--- Denoising/test_real_denoising_sidd.py
+++ Denoising/test_real_denoising_sidd.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append("/data/URDT_main/")
+sys.path.append("/data/C2F-DFT/")
 
 import numpy as np
 import argparse
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch
 import utils
 
-from basicsr.models.archs.DT_arch import DT
+from basicsr.models.archs.DFT_arch import DFT
 
 from skimage import img_as_ubyte
 from basicsr.utils import set_random_seed
@@ -91,7 +91,7 @@ def generalized_steps_overlapping(input_, model_restoration, device, betas, seq,
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Real Image Denoising using URDT')
+    parser = argparse.ArgumentParser(description='Real Image Denoising using C2F-DFT')
 
     parser.add_argument('--input_dir', default='./Datasets/test/SIDD/', type=str, help='Directory of validation images')
     parser.add_argument('--result_dir', default='./results_fine/Real_Denoising/SIDD/', type=str,
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ####### Load yaml #######
-    yaml_file = 'Options/RealDenoising_URDT_Fine.yml'
+    yaml_file = 'Options/RealDenoising_C2F-DFT_Fine.yml'
     import yaml
 
     try:
@@ -125,7 +125,7 @@ if __name__ == '__main__':
         result_dir_png = os.path.join(args.result_dir, 'png')
         os.makedirs(result_dir_png, exist_ok=True)
 
-    model_restoration = DT(**opt['network_g'])
+    model_restoration = DFT(**opt['network_g'])
 
     checkpoint = torch.load(args.weights)
     model_restoration.load_state_dict(checkpoint['params_ema'])
