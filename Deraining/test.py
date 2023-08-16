7690a1f29a4e826627839f4dc9213904a3972e1b
wlydlut <100394679+wlydlut@users.noreply.github.com>
2023-08-16 22:15:30 +0800

    Update test.py

--- Deraining/test.py
+++ Deraining/test.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("/data/URDT_main/")
+sys.path.append("/data/C2F-DFT/")
 
 import numpy as np
 import argparse
@@ -9,7 +9,7 @@ import torch
 import utils
 from natsort import natsorted
 from glob import glob
-from basicsr.models.archs.DT_arch import DT
+from basicsr.models.archs.DFT_arch import DFT
 
 from skimage import img_as_ubyte
 from basicsr.utils import set_random_seed
@@ -87,7 +87,7 @@ def generalized_steps_overlapping(input_, model_restoration, device, betas, seq,
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Image Deraining using URDT')
+    parser = argparse.ArgumentParser(description='Image Deraining using C2F-DFT')
 
     parser.add_argument('--input_dir', default='./Datasets/test/', type=str, help='Directory of validation images')
 
@@ -97,8 +97,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ####### Load yaml #######
-    yaml_file = 'Options/Deraining_URDT_Fine.yml'
-    # yaml_file = 'Options/Deraining_URDT_Coarse.yml'
+    yaml_file = 'Options/Deraining_C2F-DFT_Fine.yml'
+    # yaml_file = 'Options/Deraining_C2F-DFT_Coarse.yml'
 
     import yaml
 
@@ -114,7 +114,7 @@ if __name__ == '__main__':
     set_random_seed(seed)
 
     s = opt['network_g'].pop('type')
-    model_restoration = DT(**opt['network_g'])
+    model_restoration = DFT(**opt['network_g'])
 
     checkpoint = torch.load(args.weights)
     model_restoration.load_state_dict(checkpoint['params_ema'])
